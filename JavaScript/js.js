019e3dabe2fda70719687de5feb6665f8ea37108
Lucas Pereira <88951189+Luc45-Pereira@users.noreply.github.com>
2021-09-18 20:40:06 -0300

    Update js.js

--- JavaScript/js.js
+++ JavaScript/js.js
@@ -1,4 +1,4 @@
-function Calculadora(){
+/*function Calculadora(){
     a = document.getElementById("resultado");
     tipo = prompt("Qual o tipo de calculo?");
     valorA = prompt("Primeiro Valor:");
@@ -16,5 +16,29 @@ function Calculadora(){
     }else if(tipo == "/"){
         Total = parseInt(valorA) / parseInt(valorB);
         a.innerHTML = Total;
+    }
+}*/
+
+function Calculadora(){
+    tipo = document.getElementById("sinal");
+    sinal = tipo.options[tipo.selectedIndex].value;
+    a = document.getElementById("valor1");
+    valorA = a.value;
+    b = document.getElementById("valor2");
+    valorB = b.value;
+    console.log(sinal);
+    if(sinal == "+"){
+        Total = parseInt(valorA) + parseInt(valorB);
+        alert("O resultado é: " + Total);
+    }
+    else if(sinal == "-"){
+        Total = parseInt(valorA) - parseInt(valorB);
+        alert("O resultado é: " + Total);
+    }else if(sinal == "*"){
+        Total = parseInt(valorA) * parseInt(valorB);
+        alert("O resultado é: " + Total);
+    }else if(sinal == "/"){
+        Total = parseInt(valorA) / parseInt(valorB);
+        alert("O resultado é: " + Total);
     }
 }
